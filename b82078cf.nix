06910a10f57fa1c1719cffad26701ad961604edc
Mr Z <denver.cfman@gmail.com>
2025-09-20 21:21:04 -0600

    Update b82078cf.nix

--- b82078cf.nix
+++ b82078cf.nix
@@ -28,7 +28,7 @@
     firewall.allowedTCPPorts = [ 8888 22 ];
     #firewall.allowedUDPPorts = [ ... ];
     extraHosts = ''
-10.0.85.30 9a7e6755 rpi4-cluster-head rpi4-cluster-head.giezenconsulting.com
+10.0.83.10 9a7e6755 rpi4-cluster-head rpi4-cluster-head.giezenconsulting.com
 '';
   };
 
@@ -45,7 +45,7 @@
     token = "K109225314c2362ddcf00d33e670e2cb09150ca11226f469c52c89d1b7ee4bd3a9c::server:mytoken";
     role = "agent";
     serverAddr="https://rpi4-cluster-head.giezenconsulting.com:6443";
-    extraFlags = "--disable=servicelb";
+    #extraFlags = "--disable=servicelb";
     #disableAgent = "false";
   };
 
